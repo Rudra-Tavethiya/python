553ac7bbd3eb0380eba3b7e878094ff13d5701bb
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-02-04 09:35:28 +0530

    data types

---
+++ basic/data_type.py
@@ -0,0 +1,57 @@
+# python datatype
+
+# text type : str
+# numeric type : int float complex
+# sequence type : list tuple range
+# mapping type : dict
+# set type : set frozenset
+# boolen type : boolen
+# binary type : bytes bytearray memoryview
+# none type : none
+
+
+# numeric
+
+# a = 10
+# b = 20.63
+# c = 4+5j
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(c.real)
+# print(c.imag)
+# print(c.conjugate())
+
+
+# text
+
+# name = "rudra"
+# print(name)
+
+
+#collaction
+
+# l = [10, 20, 30, 40, 50, 50]
+# t = (10, 20, 30, 40, 50, 50)
+# s = {10, 20, 30, 40, 50, 50}
+# f = frozenset ({10, 20, 30, 40})
+# d = {"10":"python","20":"java"}
+# print(l)
+# print(t)
+# print(s)
+# print(f)
+# print(d)
+
+
+# boolean
+
+# a = True
+# print(a)
+# print(type(a))
+
+
+# none type
+
+# k = None
+# print(type(k))
