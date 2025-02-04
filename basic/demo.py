007ce533f16d57413268a99b901147cd2dbd8ac5
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-02-04 10:04:32 +0530

    4_feb_datatypes_operator

--- basic/demo.py
+++ basic/demo.py
@@ -14,4 +14,25 @@
 # print("abc\bxyz")
 
 # task print : abc \' xyz
-print("abc \\' xyz")
+# print("abc \\' xyz")
+
+
+# input function
+
+# name = input("enter name : ")
+# print(name)
+
+# a=int(input("enter number 1 : "))
+# b=int(input("enter number 2 : "))
+# print(a+b)
+
+
+# name = "i am working in dell leptop".split()
+# print(name)
+
+fname = "rudra"
+lname = "tavethiya"
+
+print("my name is {1} and my surname is {0}".format(fname,lname))
+
+print(f"my name is {fname} and surname is {lname}")
---
+++ basic/operators.py
@@ -0,0 +1,25 @@
+# arithmetic op
+# +,-,*,/,%,**,//
+
+# print(10+20)
+# print(20-10)
+# print(10*20)
+# print(20/10)
+# print(10%3)
+# print(10**2)
+# print(15//2)
+
+
+# assignment op
+# =,+=,-=,*=,/=
+
+a=10
+a+=10
+print(a)
+a-=5
+print(a)
+a*=2
+print(a)
+a/=5
+print(a)
+
