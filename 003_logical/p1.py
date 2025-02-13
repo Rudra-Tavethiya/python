8a106a05bf9f853f3f1cfea1b318ba2d5d5315ff
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-02-13 10:09:36 +0530

    partten

--- 003_logical/p1.py
+++ 003_logical/p1.py
@@ -138,28 +138,23 @@
 #  ***
 #   *
 
-# lines = 3
+# lines = 5
 # stars = 1
 # space = lines-1
-# for i in range(lines):
+# mid=(lines//2)+1
+# for i in range(1,lines+1):
 #     for k in range(space):
 #         print(" ",end="")
 #     for j in range(stars):
-#         print("*",end="")
+#             print("*",end="")
 #     print()
-#     stars+=2
-#     space-=1 
-# lines = 3
-# stars = 3
-# space = 1
-# for i in range(lines):
-#     for k in range(space):
-#         print(" ",end="")
-#     for j in range(stars):
-#         print("*",end="")
-#     print()
-#     stars-=2
-#     space+=1
+#     if i<mid:
+#         stars+=2
+#         space-=1
+#     else :
+#         stars-=2
+#         space+=1
+
 
 
 #   *
@@ -168,20 +163,119 @@
 #  * *
 #   *
 
+# lines = 5
+# stars = 1
+# space = lines-1
+# mid=(lines//2)+1
+# for i in range(1,lines+1):
+#     for k in range(space):
+#         print(" ",end="")
+#     for j in range(stars):
+#         if j==0 or j==stars-1:
+#             print("*",end="")
+#         else :
+#             print(" ",end="")
+#     print()
+#     if i<mid:
+#         stars+=2
+#         space-=1
+#     else :
+#         stars-=2
+#         space+=1
 
-lines = 3
-stars = 1
-space = lines-1
-space2 = 0
-for i in range(lines):
-    for k in range(space):
-        print(" ",end="")
-    for j in range(stars):
-        print("*",end="")
-    for l in range(space2):
-        print(" ",end="")
-    if i!=0:
-        print("*",end="")
+
+# *
+# **
+# * *
+# **
+# *
+
+# lines = 5
+# stars = 1
+# space = lines-1
+# mid=(lines//2)+1
+# for i in range(1,lines+1):
+#     for j in range(stars):
+#         if j==0 or j==stars-1:
+#             print("*",end="")
+#         else :
+#             print(" ",end="")
+#     print()
+#     if i<mid:
+#         stars+=1
+#     else :
+#         stars-=1
+
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+
+
+# 1
+# 2  3
+# 4  5  6
+# 7  8  9  10
+# 11 12 13 14 15
+
+
+# fact=1
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(fact,end=" ")
+#         fact+=1
+#     print()
+
+
+# 0
+# 10
+# 010
+# 1010
+# 01010
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print((i+j)%2,end="")
+#     print()
+
+
+# 0
+# 01
+# 010
+# 0101
+# 01010
+
+
+# zero=0
+# one=1
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         if j%2==0:
+#             print(one,end="")
+#         else:
+#             print(zero,end="")
+#     print()
+
+
+# A
+# BC
+# DEF
+# GHIJ
+# KLMNO
+
+char='A'
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(char,end="")
+        char+=1
     print()
-    space-=1 
-    space2+=1
