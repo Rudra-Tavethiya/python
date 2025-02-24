fb8c4158dc4038d4666aed4d2510d95bb2177ed4
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-02-24 08:44:24 +0530

    set_lab_practice

---
+++ 005_collection/set.py
@@ -0,0 +1,18 @@
+s1 = {10,20,30}
+s2 = {10,40,50}
+
+# s3 = s1.union(s2)
+
+# s3 = s1.update(s2)
+
+# s3 = s1.intersection(s2)
+# s1.intersection_update(s2)
+
+# s3 = s1.difference(s2)
+# s1.difference_update(s2)
+
+# s3 = s1.symmetric_difference(s2)
+# s1.symmetric_difference_update(s2)
+
+# print(s1)
+# print(s3)
