3fd3b9cfe8ca67ddf1385646a694fc65080673ce
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-03 11:22:13 +0530

    map_filter_reduce

--- 006_function/map.py
+++ 006_function/map.py
@@ -1,10 +1,10 @@
 # map 
 
-def squre(a):
-    b=int(a)
-    return b*b
+# def squre(a):
+#     b=int(a)
+#     return b*b
 
-l = [10,20,30,40,50]
+# l = [10,20,30,40,50]
 
 # l1 = []
 # for i in l:
@@ -22,6 +22,35 @@ l = [10,20,30,40,50]
 
 # lambda
 
-l = [1,2,3,4,5]
-l1 = map(lambda x:x*x,l)
-print(list(l1))
+# l = [1,2,3,4,5]
+# l1 = map(lambda x:x*x,l)
+# print(list(l1))
+
+# a = [10,20,30,40,50,1,3,4,5]
+# b = [1,2,3,4,5]
+
+# c  = map(lambda x,y:x+y,a,b)
+# print(list(c))
+
+# c = filter(lambda x:x%2!=0,a)
+# print(list(c))
+
+
+# from math import sqrt
+
+data = [1, 4, 6, 8, 9, 10, 12, 16, 81, 23, 36, 48]  
+# def isPreSquare(i):
+#     return sqrt(i).is_integer()
+
+# k = filter(isPreSquare,data)
+# print(list(k))
+
+
+from functools import reduce
+
+def add(x,y):
+    return x*y
+
+c = reduce(add,data)
+c = reduce(lambda x,y:x+y,data)
+print(c)
