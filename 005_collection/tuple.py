c6a1101782b1df5df0fc2c3c796e999d4d5760ee
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-03 11:06:29 +0530

    tuple

---
+++ 005_collection/tuple.py
@@ -0,0 +1,42 @@
+# t = ("python","php","android","ios")
+
+
+# l = ["Hello"]
+# print(l)
+# tpl = ("Hello",)
+# print(tpl)
+
+# l = list(t)
+# l.append("SQL")
+# t = tuple(l)
+# print(t)
+
+# x = ("test","tech")
+# y = ("abc","xyz")
+
+# x +=y
+# print(x)
+
+
+
+fruits = ("apple", "banana", "cherry")
+
+# (a,*b) = fruits
+
+# print(b)
+
+# for i in fruits:
+#     print(i)
+
+# for i in range(len(fruits)):
+#     print(fruits[i])
+
+
+
+t1 = (10,20,30)
+t2 = (80,90,70)
+
+t3 = t1+t2
+print(t3)
+t3  = t1*3
+print(t3)
