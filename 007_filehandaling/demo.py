85ebb5710ebce6faad27c855085a78b2965dcb78
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-10 08:21:39 +0530

    file handaling

---
+++ 007_filehandaling/demo.py
@@ -0,0 +1,20 @@
+# file = open("text.txt",'r')
+# print(file.read())
+
+
+# file = open("text.txt",'w')
+# file.write("python programing")
+
+
+# file = open("text.txt",'a')
+# file.write("go to home")
+
+
+# file = open("text.txt",'r')
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+
+
+file = open("text.txt",'r')
+print(file.readlines())
