9e9564b612d667545a139cfc675f7d066a87206e
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-10 08:27:01 +0530

    file handling

--- 007_filehandaling/demo.py
+++ 007_filehandaling/demo.py
@@ -10,11 +10,24 @@
 # file.write("go to home")
 
 
+
 # file = open("text.txt",'r')
 # print(file.readline())
 # print(file.readline())
 # print(file.readline())
 
 
-file = open("text.txt",'r')
-print(file.readlines())
+# file = open("text.txt",'r')
+# while True:
+#     p = file.readline()
+#     print(p)
+#     if not p:
+#         break
+
+
+# file = open("text.txt",'r')
+# print(file.readlines())
+
+with open("text.txt",'r') as f:
+    p = f.read()
+    print(p)
