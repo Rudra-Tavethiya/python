88ff09db74690033c675ece5e1c9b19f768cf793
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-17 10:36:49 +0530

    assignment

---
+++ ASS_PYTHON/module_2/lab/1_1.py
@@ -0,0 +1,5 @@
+# Write a Python program to create a list with elements of multiple data types (integers, strings, floats, etc.). 
+
+
+list = [1,"python",5.2,2,8.1,3]
+print(list)
---
+++ ASS_PYTHON/module_2/lab/1_2.py
@@ -0,0 +1,9 @@
+# Write a Python program to access elements at different index positions. 
+
+
+list = [1,"python",5.2,2,8.1,3]
+print(list[0])
+print(list[-1])
+print(list[1:])
+print(list[::1])
+print(list[::-1])
