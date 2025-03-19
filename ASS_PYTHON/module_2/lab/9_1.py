a096ed0d0d96bf2a8ed200028248ad040032c1ba
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-19 09:19:34 +0530

    assignment

---
+++ ASS_PYTHON/module_2/lab/9_1.py
@@ -0,0 +1,9 @@
+# Write a Python program to import the math module and use functions like sqrt(), ceil(), floor(). 
+
+
+
+import math
+
+print(math.sqrt(25))
+print(math.ceil(2.1))
+print(math.floor(2.9))
---
+++ ASS_PYTHON/module_2/lab/9_2.py
@@ -0,0 +1,7 @@
+# Write a Python program to generate random numbers using the random module. 
+
+
+import random
+
+r = random.randint(100,999)
+print(r)
