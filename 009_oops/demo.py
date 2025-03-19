6fe5f6e6eb07a7c83a82928039ac051433a1a0e7
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-19 08:39:49 +0530

    class

--- 009_oops/demo.py
+++ 009_oops/demo.py
@@ -2,13 +2,24 @@ class write:
 
     id = 101
     name = "python"
+
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+
+
     def display(self):
         print("display calling....",self.id)
 
     def show(self):
         print("show calling....",self.name)
 
-w = write()
+w1 = write(11,"rudra")
 
-w.display()
-w.show()
+w1.display()
+w1.show()
+
+w2 = write(12,"hello")
+
+w2.display()
+w2.show()
