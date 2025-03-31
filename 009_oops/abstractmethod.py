2ee5a65475447b11f0c3ce1d91b6945e3b7e8050
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-03-31 11:35:04 +0530

    abstruct method

--- 009_oops/abstractmethod.py
+++ 009_oops/abstractmethod.py
@@ -1,3 +1,37 @@
-import ABC from abstractmethod,ABC
+from abc import abstractmethod,ABC
 
 
+class account(ABC):
+
+    balance=0
+
+    @abstractmethod
+    def deposit(self):
+        pass
+
+    def checkbalance(self):
+        print("current balance : ",self.balance)
+
+
+class savingaccount(account):
+
+    def deposit(self,amt):
+        self.balance+=amt
+
+
+class loanaccount(account):
+
+    def deposit(self,amount):
+        self.balance-=amount
+    
+
+s = savingaccount()
+s.checkbalance()
+s.deposit(2000)
+s.checkbalance()
+print("************************")
+l = loanaccount()
+l.balance = 10000
+l.checkbalance()
+l.deposit(5000)
+l.checkbalance()
