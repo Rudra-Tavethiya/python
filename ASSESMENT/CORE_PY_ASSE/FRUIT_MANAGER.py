4d510f02370551ed25da585dd33f3b6b16cde65a
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-04-08 11:20:55 +0530

    CORE PYTHON ASSESMENT

---
+++ ASSESMENT/CORE_PY_ASSE/FRUIT_MANAGER.py
@@ -0,0 +1,49 @@
+
+
+
+def manage_options():
+    data = {}
+    con='y'
+    while con!='n':
+
+        print("""
+              Fruit Market Manager
+
+              1) Add Fruit Stock
+              2) View Fruit Stock
+              3) Update Fruit Store
+
+        """)
+    
+        choice = int(input("Enter Your Choice : "))
+    
+        if choice==1:
+            
+            print("ADD FRUIT STOCK")
+            f_name = input("Enter Fruit Name : ")
+            f_qty = int(input("Enter Qty (in KG) : "))
+            f_prise = int(input("Enter Prise : "))
+
+            data.update({f_name:{"qty":f_qty,"prise":f_prise}})
+
+            con = input("do you want to perform more operations : press y fro yes and press n for no : ")
+            if con=='y':
+                print("""
+                      FRUIT DATA ADDED IN STOCK
+                      """)
+                pass
+            elif con=='n':
+                print("""
+                      DATA ADDED IN STOCK
+                      """)
+                break
+            else : 
+                print("""
+                      YOU ENTER INVALID INPUT!!!
+                      """)
+                break
+
+        elif choice==2:
+            print("view fruit stock")
+            print(data)
+            break
---
+++ ASSESMENT/CORE_PY_ASSE/FRUIT_STORE.py
@@ -0,0 +1,30 @@
+
+
+
+import FRUIT_MANAGER as f
+import CUSTOMER as c
+
+
+print("""
+      WELCOME TO FRUIT MARKET
+            
+        1) Manager
+        2) Customer
+      
+        3) EXIT
+""")
+
+choice = int(input("Enter Your Role : "))
+
+if choice==1:
+    f.manage_options()
+elif choice==2:
+    c.customer_operation()
+elif choice==3:
+    print("""
+          YOU ARE EXIT NOW
+          """)
+else:
+    print("""
+          Invalid Choice !!!
+          """)
