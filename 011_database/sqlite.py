29623495b6b5684860003de819ed332e35adb3e7
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-04-09 10:32:49 +0530

    sqlite

--- 011_database/sqlite.py
+++ 011_database/sqlite.py
@@ -8,8 +8,17 @@ mydb = sqlite3.connect("data.db")
 
 # mydb.execute("insert into student values(1,'rudra','r@gmail.com',123456)")
 
-data = mydb.execute("select * from student")
-print(data)
+# mydb.execute("insert into student values(2,'fenil','f@gmail.com',654321)")
 
+r = mydb.execute("select * from student")
+print(r)
+
+# for data in r.fetchall():
+#     print(data)
+
+for data in r.fetchall():
+    for i in data:
+        print(i,end="   ")
+    print()
 
 mydb.commit()
