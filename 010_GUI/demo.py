b202d519d0810d4effd53206048a29da665b4ba4
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-04-09 09:04:35 +0530

    guit connection with mysql

--- 010_GUI/demo.py
+++ 010_GUI/demo.py
@@ -1,5 +1,34 @@
 from tkinter import *
 
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="9316977670",
+    port=3306,
+    database="1_fab"
+)
+
+cursor = mydb.cursor()
+
+def add_data():
+
+    name = e1.get()
+    email = e2.get()
+    paswword = e3.get()
+
+    qry = "insert into data values(%s,%s,%s,%s)"
+    val = (0,name,email,paswword)
+
+    cursor.execute(qry,val)
+    mydb.commit()
+
+    e1.delete(0,END)
+    e2.delete(0,END)
+    e3.delete(0,END)
+
+    
 
 root = Tk()
 root.geometry("500x500")
@@ -26,11 +55,14 @@ l1 = Label(text="Name").place(x=100,y=100)
 l2 = Label(text="Email").place(x=100,y=150)
 l3 = Label(text="Password").place(x=100,y=200)
 
-e1 = Entry().place(x=200,y=100)
-e2 = Entry().place(x=200,y=150)
-e3 = Entry().place(x=200,y=200)
+e1 = Entry()
+e1.place(x=200,y=100)
+e2 = Entry()
+e2.place(x=200,y=150)
+e3 = Entry()
+e3.place(x=200,y=200)
 
-Button(text="Submit").place(x=250,y=250)
+Button(text="Submit",command=add_data).place(x=250,y=250)
 
 
 
