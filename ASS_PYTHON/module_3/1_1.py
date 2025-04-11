0bd80b5ebb6a0b67cca3ea54c456b19cbd658380
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-04-11 11:22:50 +0530

    assignment

---
+++ ASS_PYTHON/module_3/1_1.py
@@ -0,0 +1,10 @@
+# Write a Python program to print a formatted string using print() and f-string.
+
+
+
+print("hello, world!!")
+
+
+h = "hello,"
+w = "world!!"
+print(f"{h} {w}")
---
+++ ASS_PYTHON/module_3/2_1.py
@@ -0,0 +1,11 @@
+# Write a Python program to read a name and age from the user and print a formatted output.
+
+
+
+name = input("enter name : ")
+age = int(input("enter age : "))
+
+print()
+print("now printing the details : ")
+print("name : ",name)
+print("age : ",age)
