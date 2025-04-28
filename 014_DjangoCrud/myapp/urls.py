3eb2211cc75bc5fd624b2d5bb6378a25bee98668
Rudra-Tavethiya <rudratavethiya2004@gmail.com>
2025-04-28 08:48:26 +0530

    crud oppration

--- 014_DjangoCrud/myapp/urls.py
+++ 014_DjangoCrud/myapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from myapp.views import *
 
 urlpatterns = [
-    path("",index,name="index")
+    path("",index,name='index'),
+    path("register",register,name="register")
 ]
--- 014_DjangoCrud/myapp/views.py
+++ 014_DjangoCrud/myapp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from myapp.models import *
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 
+def register(request):
+    print("ok")
+    if request.method=='POST':
+        data = request.POST
+        username = data.get("uname")
+        email = data.get("email")
+        phone = data.get("phone")
+        age = data.get("age")
+
+        st = student.objects.create(username=username,email=email,phone=phone,age=age)
+
+        if(st):
+            return render(request,'index.html',{'msg':"Registration successful"})
+
+    return render(request,"index.html")
